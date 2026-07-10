d276d335f00b0fcd9d63464a8b532ab7c4393343
Rony <49360849+r0ny123@users.noreply.github.com>
2026-07-10 18:35:35 +0530

    Apply suggestions from code review
    Co-authored-by: Daniel Plohmann <daniel.plohmann@mailbox.org>

--- config.py
+++ config.py
@@ -150,7 +150,7 @@ settings = SettingsWrapper()
 
 
 # --- Original Config Constants ---
-VERSION = "1.1.5"
+VERSION = "1.1.7"
 # relevant paths
 CONFIG_FILE_PATH = os.path.abspath(__file__)
 PROJECT_ROOT = os.path.dirname(CONFIG_FILE_PATH)
